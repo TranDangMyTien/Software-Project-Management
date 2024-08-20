0232994ee9015fa7d314e216125f2e75c185dff2
TranDangMyTien <106966807+TranDangMyTien@users.noreply.github.com>
2024-08-20 18:22:49 +0700

    Lập trình giao diện trang đăng ký và xét duyệt thẻ giữ xe

--- apartment/src/components/layout/Footer.jsx
+++ apartment/src/components/layout/Footer.jsx
@@ -1,19 +1,31 @@
-import React from "react"
-import { Col, Container, Row } from "react-bootstrap"
+import React from 'react';
+import './Footer.css'; // File CSS nếu bạn muốn thêm style riêng
 
 const Footer = () => {
-	let today = new Date()
-	return (
-		<footer className="bg-dark text-light py-3 footer">
-			<Container>
-				<Row>
-					<Col xs={12} md={12} className="text-center">
-						<p className="mb-0"> &copy; {today.getFullYear()} TNVV Aparment</p>
-					</Col>
-				</Row>
-			</Container>
-		</footer>
-	)
-}
+    return (
+        <footer className="footer">
+            <div className="footer__content">
+                <div className="footer__section">
+                    <h4>Chung Cư TNVV</h4>
+                    <p>&copy; 2024 Chung Cư TNVV. All rights reserved.</p>
+                </div>
+                <div className="footer__section">
+                    <h4>Thành Viên</h4>
+                    <ul>
+                        <li><a>Nguyễn Thị Hiền Vy</a></li>
+                        <li><a>Nguyễn Thị Mỹ Vân</a></li>
+                        <li><a>Nguyễn Hoàng Nhi</a></li>
+                        <li><a>Trần Đặng Mỹ Tiên</a></li>
+                    </ul>
+                </div>
+                <div className="footer__section">
+                    <h4>Liên Hệ</h4>
+                    <p>Trường Đại học Mở thành phố Hồ Chí Minh</p>
+                    <p>Khoa Công Nghệ Thông Tin</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
